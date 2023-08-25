1e35ce7fd7928c745690cfe8c75c5f054e3b7393
Son Hoang <sonquhoang@gmail.com>
2023-08-25 14:35:14 -0700

    Modified nav bar to have the different pages and formatted them horizontally, added a tips page

--- react-frontend/src/components/Navigation/index.js
+++ react-frontend/src/components/Navigation/index.js
@@ -4,20 +4,37 @@ import { useSelector } from 'react-redux';
 import ProfileButton from './ProfileButton';
 import './Navigation.css';
 
-function Navigation({ isLoaded }){
+function Navigation({ isLoaded }) {
 	const sessionUser = useSelector(state => state.session.user);
 
 	return (
-		<ul>
-			<li>
-				<NavLink exact to="/">Home</NavLink>
-			</li>
-			{isLoaded && (
-				<li>
-					<ProfileButton user={sessionUser} />
-				</li>
-			)}
-		</ul>
+		<nav className="navbar">
+			<div className="nav-container">
+				<ul className="nav-left">
+					<li>
+						<NavLink exact to="/">Home</NavLink>
+					</li>
+					<li>
+						<NavLink to="/forecast">Forecast</NavLink>
+					</li>
+					<li>
+						<NavLink to="/stories">Stories</NavLink>
+					</li>
+					<li>
+						<NavLink to="/tips">Tips</NavLink>
+					</li>
+				</ul>
+				<ul className="nav-right">
+					<div className="profile-button-container">
+					{isLoaded && (
+						<li>
+							<ProfileButton user={sessionUser} />
+						</li>
+					)}
+					</div>
+				</ul>
+			</div>
+		</nav>
 	);
 }
 
