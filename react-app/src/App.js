4190286a91afa4a5b0c338b63496cda06059917b
Son Hoang <sonquhoang@gmail.com>
2023-08-25 08:28:03 -0700

    Starter code skeleton

---
+++ react-app/src/App.js
@@ -0,0 +1,38 @@
+import React, { useState, useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { Route, Switch } from "react-router-dom";
+import SignupFormPage from "./components/SignupFormPage";
+import LoginFormPage from "./components/LoginFormPage";
+import { authenticate } from "./store/session";
+import Navigation from "./components/Navigation";
+// import ProtectedRoute from "./components/auth/ProtectedRoute"
+
+function App() {
+  const dispatch = useDispatch();
+  const [isLoaded, setIsLoaded] = useState(false);
+  useEffect(() => {
+    dispatch(authenticate()).then(() => setIsLoaded(true));
+  }, [dispatch]);
+
+  return (
+    <>
+      <Navigation isLoaded={isLoaded} />
+      {isLoaded && (
+        <Switch>
+          <Route path="/login" >
+            <LoginFormPage />
+          </Route>
+          <Route path="/signup">
+            <SignupFormPage />
+            {/* Example of using protected route */}
+            {/* Checks for a logged in user [Front-end protection] */}
+            {/* <ProtectedRoute path ="/feed" */}
+            {/* <Feed /> */}
+          </Route>
+        </Switch>
+      )}
+    </>
+  );
+}
+
+export default App;
