90204beca386df697abecbadfc35f97efac84912
Son Hoang <sonquhoang@gmail.com>
2023-08-28 08:32:21 -0700

    Able to create tips, and retrieve tips from db

--- react-frontend/src/components/Tips/index.js
+++ react-frontend/src/components/Tips/index.js
@@ -1,10 +1,12 @@
 import React, { useState } from 'react';
-import Navigation from '../Navigation';
+// import Navigation from '../Navigation';
 import "./tips.css"
 import TipsAddModal from '../TipsAddModal';
+import GetAllTips from './GetAllTips';
 
 function Tips() {
     const [isModalOpen, setIsModalOpen] = useState(false)
+    const [submittedTips, setSubmittedTips] = useState([])
 
     const handleCloseModal = () => {
         setIsModalOpen(false);
@@ -14,15 +16,38 @@ function Tips() {
         setIsModalOpen(true)
     }
 
+    const handleAddTip = (newTip) => {
+        setSubmittedTips([...submittedTips, newTip])
+    }
+
     return (
         <>
             <div className="tips-container">
-                <h1>Testing out the Tips</h1>
-            </div>
-
             <div className="tip-button">
                 <button onClick={handleOpenModal}>Add a Tip!</button>
-                {isModalOpen && <TipsAddModal onClose={handleCloseModal} />}
+                {isModalOpen && <TipsAddModal onClose={handleCloseModal} onAddTip={handleAddTip} />}
+            </div>
+
+            <div className="submitted-tips">
+                <h2>Submitted Tips:</h2>
+                <ul>
+                    {submittedTips.map((tip, index) => (
+                        <li key={index}>
+                            <div className="tip-content">
+                                {tip.title}<br />
+                                {tip.weather_category}<br />
+                                {tip.tip}
+                            </div>
+                        </li>
+                    ))}
+            </ul>
+        </div >
+
+        <div className="all-tips">
+                <ul>
+                    <GetAllTips />
+                </ul>
+            </div>
             </div>
         </>
     )
