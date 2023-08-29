a6aa806dc8faadc377ee3a1c925306b25ff9f2ce
Son Hoang <sonquhoang@gmail.com>
2023-08-29 13:20:00 -0700

    Modified tables and migrations to have prod code

--- flask-backend/app/models/story_comments.py
+++ flask-backend/app/models/story_comments.py
@@ -4,6 +4,11 @@ from .db import db, environment, SCHEMA, add_prefix_for_prod
 
 class StoryComments(db.Model):
     __tablename__ = "story_comments"
+
+    if environment == "production":
+        __table_args__ = {'schema': SCHEMA}
+
+
     id = db.Column(db.Integer, primary_key=True)
     story_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod('stories.id')), nullable=False)
     user_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod('users.id')), nullable=False)
