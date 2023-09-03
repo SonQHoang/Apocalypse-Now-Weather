689a17ad100129117116276f25b9b2db6a1a090f
samBoyle <sboyle05@gmail.com>
2023-09-02 17:18:00 -0700

    profile button styled

--- react-app/src/components/Navigation/ProfileButton.js
+++ react-app/src/components/Navigation/ProfileButton.js
@@ -48,36 +48,37 @@ function ProfileButton({ user }) {
 
   return (
     <>
-      <button id="mainprofilebutton" onClick={openMenu}>
+      <button onClick={openMenu}>
        <img id="usericon" src="/icons/user.png" alt="User Profile"/>
       </button>
-      <div className={user ? 'user-logged-in' : 'user-not-logged-in'} ref={ulRef}>
+      <div className={ulClassName} id={user ? 'user-logged-in' : 'user-not-logged-in'} ref={ulRef}>
         {user ? (
           <>
-            <div>User: {user.username}</div>
-            <div>Email:{user.email}</div>
-            <div className="dropdownbut"><NavLink exact to='/stories/manage'><button>My Stories</button></NavLink></div>
-             <div className="dropdownbut"><NavLink exact to='/tips/manage'><button>My Tips</button></NavLink></div>
-             <div className="dropdownbut"><NavLink exact to={`/survivors/current`}><button>My Profile</button></NavLink></div>
-              <button onClick={handleLogout}>Log Out</button>
+            <li>User:  {user.username}</li>
+            <li>Email:  {user.email}</li>
+            <li>
+              <NavLink exact to='/stories/manage'><button>My Stories</button></NavLink> </li>
+             <li><NavLink exact to='/tips/manage'><button>My Tips</button></NavLink></li>
+             <li><NavLink exact to={`/survivors/current`}><button>My Profile</button></NavLink></li>
+            <li><button onClick={handleLogout}>Log Out</button></li>
 
           </>
         ) : (
           <>
-            <OpenModalButton
+            <li><OpenModalButton
               buttonText="Log In"
               onItemClick={closeMenu}
               modalComponent={<LoginFormModal />}
-            />
+            /></li>
 
             {/* <OpenModalButton
               buttonText="Sign Up"
               onItemClick={closeMenu}
               modalComponent={<SignUpFormModal/>}
             /> */}
-            <button id="signupnavbutton">
+            <li><button id="signupnavbutton">
               <Link to="/signup" onClick={closeMenu}>Sign Up</Link>
-            </button>
+            </button></li>
           </>
         )}
       </div>
