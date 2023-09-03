210c8c5c8fd544fbeacb10784d3d8ca7278ff992
Jonathan Wofford <jonathanbrodie91@gmail.com>
2023-09-03 19:57:35 +0200

    added in five day forcast

--- react-app/src/components/FiveDayForcast/fiveDayForcast.js
+++ react-app/src/components/FiveDayForcast/fiveDayForcast.js
@@ -28,73 +28,92 @@ const FiveDayForcast = () =>{
     let weatherData;
 
     const setWeatherData = (weathercode) => {
-      weatherData = {}
+      weatherData = {};
       let convertedDailyWeather = [];
       let dailyWeather = [];
       for (let i = 0; i < weathercode.length; i++) {
-        convertedDailyWeather.push(apocWeatherConverter(weathercode[i]));
+        let convertedCode = apocWeatherConverter(weathercode[i])
+        convertedDailyWeather.push(convertedCode);
       }
       convertedDailyWeather.forEach((day) => {
         dailyWeather.push(day.name);
       });
+
       weatherData.dailyWeather = dailyWeather;
       return weatherData;
     };
-    if(userForcast && userForcast.time) {
-        time = userForcast.time
-        weathercode = userForcast.weathercode
-        maxTemp = userForcast.temperature_2m_max
-        setWeatherData(weathercode)
+
+    if (userForcast && userForcast.time) {
+      time = userForcast.time;
+
+      time.forEach((thing) => {
+        let splitThing = thing.split("-");
+        let spliceThing = splitThing.splice(1);
+        let newtime = spliceThing.join("/");
+        time.push(newtime);
+      });
+
+      weathercode = userForcast.weathercode;
+      maxTemp = userForcast.temperature_2m_max;
+      setWeatherData(weathercode);
     }
 
-    console.log(weatherData)
-
-
-
-    if(weatherData && weatherData.dailyWeather) {
-        const dailyWeather = weatherData.dailyWeather
+    if (weatherData && weatherData.dailyWeather) {
+      const date = time;
+      const dailyWeather = weatherData.dailyWeather;
       return (
-        <>
-          <h1>5 Day Forcast</h1>
+          <>
+          <div className='heading-container'>
+            <h1>5 Day Forcast</h1>
+
+          </div>
           <div className="forcast-container">
-
             <>
-                <div className='forcast-container-weather-temp'>
-                    {dailyWeather[0]}
-                </div>
-                <div className='forcast-container-weather-temp'>
-                    {dailyWeather[1]}
-                </div>
-                <div className='forcast-container-weather-temp'>
-                    {dailyWeather[2]}
-                </div>
-                <div className='forcast-container-weather-temp'>
-                    {dailyWeather[3]}
-                </div>
-                <div className='forcast-container-weather-temp'>
-                    {dailyWeather[4]}
-                </div>
+              <div className="forcast-container-weather">
+                <div className='forcast-container-weather-date'>{date[5]}</div>
+                <div className='forcast-container-weather-weather'>{dailyWeather[0]}</div>
+                <div>Temp: {maxTemp[0]}</div>
+              </div>
+              <div className="forcast-container-weather">
+                <div className='forcast-container-weather-date'>{date[6]}</div>
+                <div className='forcast-container-weather-weather'>{dailyWeather[1]}</div>
+                <div>Temp: {maxTemp[1]}</div>
+              </div>
+              <div className="forcast-container-weather">
+                <div className='forcast-container-weather-date'>{date[7]}</div>
+                <div className='forcast-container-weather-weather'>{dailyWeather[2]}</div>
+                <div>Temp: {maxTemp[2]}</div>
+              </div>
+              <div className="forcast-container-weather">
+                <div className='forcast-container-weather-date'>{date[8]}</div>
+                <div className='forcast-container-weather-weather'>{dailyWeather[3]}</div>
+                <div>Temp: {maxTemp[3]}</div>
+              </div>
+              <div className="forcast-container-weather">
+                <div className='forcast-container-weather-date'>{date[9]}</div>
+                <div className='forcast-container-weather-weather'>{dailyWeather[4]}</div>
+                <div>Temp: {maxTemp[4]}</div>
+              </div>
             </>
-
           </div>
         </>
       );
     } else {
-        // console.log('DAILY WEATHER',weatherData)
-        return (
+      // console.log('DAILY WEATHER',weatherData)
+      return (
         <>
-            {/* <h1>5 Day Forcast</h1> */}
-            <div className='forcast-container'>
-                {/* {weatherData.map((dailyMaxTemp, dailyWeather) = (
+          {/* <h1>5 Day Forcast</h1> */}
+          <div className="forcast-container">
+            {/* {weatherData.map((dailyMaxTemp, dailyWeather) = (
                     <>
                     <div className='forcast-container-weather'>{dailyWeather}</div>
                     <div className='forcast-container-weather'>{dailyMaxTemp}</div>
                     </>
                 ))} */}
-            </div>
+          </div>
         </>
-    )
-}
+      );
+    }
 }
 
 
